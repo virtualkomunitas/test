4134bf3ff4a2dec43329e9893a4fe1d0d7c6e9a4
virtualkomunitas <101849803+virtualkomunitas@users.noreply.github.com>
2022-03-26 22:43:44 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NotReallyShikhar/YukkiMusicBot",
+    "https://github.com/virtualkomunitas/YukkiMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
