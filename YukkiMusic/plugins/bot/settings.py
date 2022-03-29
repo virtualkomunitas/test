c0fc6dd0c9b057dd9c75215d4a574092757700de
virtualkomunitas <101849803+virtualkomunitas@users.noreply.github.com>
2022-03-29 13:48:02 +0700

    Update settings.py

--- YukkiMusic/plugins/bot/settings.py
+++ YukkiMusic/plugins/bot/settings.py
@@ -512,8 +512,8 @@ async def cleanmode_mark(client, CallbackQuery, _):
         if await is_suggestion(CallbackQuery.message.chat.id):
             sug = True
         cle = None
-        if await is_cleanmode_on(CallbackQuery.message.chat.id):
-            await cleanmode_off(CallbackQuery.message.chat.id)
+        if await is_cleanmode_off(CallbackQuery.message.chat.id):
+            await cleanmode_on(CallbackQuery.message.chat.id)
         else:
             await cleanmode_off(CallbackQuery.message.chat.id)
             cle = True
@@ -524,7 +524,7 @@ async def cleanmode_mark(client, CallbackQuery, _):
     if command == "COMMANDELMODE":
         cle = None
         sta = None
-        if await is_cleanmode_on(CallbackQuery.message.chat.id):
+        if await is_cleanmode_off(CallbackQuery.message.chat.id):
             cle = True
         sug = None
         if await is_suggestion(CallbackQuery.message.chat.id):
@@ -538,7 +538,7 @@ async def cleanmode_mark(client, CallbackQuery, _):
     if command == "SUGGESTIONCHANGE":
         cle = None
         sta = None
-        if await is_cleanmode_on(CallbackQuery.message.chat.id):
+        if await is_cleanmode_off(CallbackQuery.message.chat.id):
             cle = True
         if await is_commanddelete_on(CallbackQuery.message.chat.id):
             sta = True
