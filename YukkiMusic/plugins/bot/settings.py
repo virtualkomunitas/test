41d927ff3fff1a97922fca75f30a36c4bff4865a
virtualkomunitas <101849803+virtualkomunitas@users.noreply.github.com>
2022-03-29 11:04:15 +0700

    Update settings.py

--- YukkiMusic/plugins/bot/settings.py
+++ YukkiMusic/plugins/bot/settings.py
@@ -516,7 +516,7 @@ async def cleanmode_mark(client, CallbackQuery, _):
             await cleanmode_off(CallbackQuery.message.chat.id)
         else:
             await cleanmode_on(CallbackQuery.message.chat.id)
-            cle = True
+            cle = False
         buttons = cleanmode_settings_markup(_, status=cle, dels=sta, sug=sug)
         return await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
